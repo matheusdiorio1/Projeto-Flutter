d7d15d82470ed37b7100941eeecbc116f9bdbb4e
Matheus Diório <matheusdiorio8@gmail.com>
2026-04-09 19:58:34 -0300

    Remoção de variável inútil

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       home: LoginPage(),
