497335901f96009bb971490101b8187e500ee68b
Matheus Diório <matheusdiorio8@gmail.com>
2026-04-17 12:04:34 -0300

    Criação da pasta theme e validação para cadastro

--- login_app/lib/screens/cadastro_screen.dart
+++ login_app/lib/screens/cadastro_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/screens/login_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:login_app/theme/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,11 +26,29 @@ class CadastroScreen extends StatefulWidget {
 }
 
 class _CadastroPageState extends State<CadastroScreen> {
+  Color corNome = Colors.grey;
   String mensagemErro = "";
   final TextEditingController nomeController = TextEditingController();
+  final TextEditingController telefoneController = TextEditingController();
+  final TextEditingController dataNascimentoController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController senhaController = TextEditingController();
   final TextEditingController senhaConfirmaController = TextEditingController();
+
+  bool nomeValido(String nome) {
+    final regex = RegExp(r'^[A-Za-zÀ-ÿ ]+$');
+
+    return nome.trim().length >= 3 && regex.hasMatch(nome);
+  }
+
+  bool emailValido(String email) {
+    final regex = RegExp(r'^[^@]+@[^@]+\.[^@]+$');
+
+    if (!regex.hasMatch(email)) return false;
+    if (email.length < 6) return false;
+
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,10 +71,31 @@ class _CadastroPageState extends State<CadastroScreen> {
             TextField(
               controller: nomeController,
               decoration: InputDecoration(
-                labelText: 'Nome',
+                labelText: 'Nome Completo',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: corNome),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            TextField(
+              controller: dataNascimentoController,
+              decoration: InputDecoration(
+                labelText: 'Data de Nascimento',
                 border: OutlineInputBorder(),
               ),
             ),
+            const SizedBox(height: 20),
+
+            TextField(
+              controller: telefoneController,
+              decoration: InputDecoration(
+                labelText: 'Telefone (DDD e Número)',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
             const SizedBox(height: 20),
             TextField(
               controller: emailController,
@@ -74,8 +114,9 @@ class _CadastroPageState extends State<CadastroScreen> {
               ),
             ),
             const SizedBox(height: 20),
-            const TextField(
+            TextField(
               obscureText: true,
+              controller: senhaConfirmaController,
               decoration: InputDecoration(
                 labelText: 'Confirmar Senha',
                 border: OutlineInputBorder(),
@@ -84,17 +125,35 @@ class _CadastroPageState extends State<CadastroScreen> {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
+                mensagemErro = '';
+                print('Clicou no botão');
+                
                 if (senhaController.text != senhaConfirmaController.text) {
-                  mensagemErro = 'Senhas não coincidem';
+                  setState(() {
+                    mensagemErro = 'Senhas não coincidem';
+                  });
+                } else if (senhaController.text.length < 8) {
+                  setState(() {
+                  mensagemErro = 'Senha inválida, menos de 8 carácteres';
+                  });
+                } else if (!emailValido(emailController.text)) {
+                  setState(() {
+                  mensagemErro = 'E-mail inválido';
+                  });
+                } else if (!nomeValido(nomeController.text)) {
+                  setState(() {
+                  corNome = AppColors.error;
+                  });
                 } else {
-                  String SenhaCheck = 'Ok';
+                  await FirebaseFirestore.instance.collection('clientes').add({
+                    'nome': nomeController.text,
+                    'email': emailController.text,
+                    'senha_hash': senhaController.text,
+                  });
+                  setState(() {
+                  mensagemErro = 'Cadastrado com sucesso!';
+                  });
                 }
-                await FirebaseFirestore.instance.collection('clientes').add({
-                  'nome': nomeController.text,
-                  'email': emailController.text,
-                  'senha_hash': senhaController.text,
-                });
-                print("Inserido com sucesso!");
               },
               child: const Text('Criar Conta'),
             ),
---
+++ login_app/lib/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF000000);
+  static const success = Color(0xFF2ECC71);
+  static const error = Color(0xFFE74C3C);
+  static const background = Color(0xFFFFFFFF);
+  static const text = Color(0xFF333333);
+}
